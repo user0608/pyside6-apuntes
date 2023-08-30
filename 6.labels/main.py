10b65387f030a93490e0d60b7603b999a0758f59
Kevin Saucedo <user002_@outlook.com>
2023-08-30 14:22:37 -0500

    6. uso de etiquetas

---
+++ 6.labels/main.py
@@ -0,0 +1,24 @@
+from PySide6.QtWidgets import QApplication,   QMainWindow, QLabel
+from PySide6.QtGui import QFont
+from PySide6.QtCore import Qt
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.initialize()
+
+    def initialize(self):
+        self.lable = QLabel("Soy una etiqueta")
+        self.lable.setFont(QFont("Comic Sans MS", 24))
+        self.lable.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.setCentralWidget(self.lable)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
