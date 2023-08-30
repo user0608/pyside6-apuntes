320f1c128408449895b9c5f98cfd574fdb0691a9
Kevin Saucedo <user002_@outlook.com>
2023-08-29 23:00:21 -0500

    1. usando QMainWindow y agregando un boton

--- 1.start/main.py
+++ 1.start/main.py
@@ -1,9 +1,14 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 import sys
 
 
 app = QApplication(sys.argv)
-window = QWidget()
+window = QMainWindow()
+window.setWindowTitle("Hola, soy el titulo")
+
+button = QPushButton("Tocame")
+window.setCentralWidget(button)
+
 window.show()
 
 if __name__ == "__main__":
