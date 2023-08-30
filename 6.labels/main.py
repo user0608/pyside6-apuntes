3def8620e5fad6e46b8b28b9051de1b56e56e2a9
Kevin Saucedo <user002_@outlook.com>
2023-08-30 14:44:57 -0500

    6. agregando una imagen a las lable

--- 6.labels/main.py
+++ 6.labels/main.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication,   QMainWindow, QLabel
-from PySide6.QtGui import QFont
+from PySide6.QtGui import QFont, QPixmap
 from PySide6.QtCore import Qt
 import sys
 
@@ -12,6 +12,7 @@ class MainWindow(QMainWindow):
     def initialize(self):
         self.lable = QLabel("Soy una etiqueta")
         self.lable.setFont(QFont("Comic Sans MS", 24))
+        self.lable.setPixmap(QPixmap("6.labels/gato.jpg"))
         self.lable.setAlignment(
             Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
         self.setCentralWidget(self.lable)
