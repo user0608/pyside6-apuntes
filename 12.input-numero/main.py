fabe29870554a191174b8829b1a69edb9f54064a
Kevin Saucedo <user002_@outlook.com>
2023-08-31 22:44:48 -0500

    12. input de numeros decimales

---
+++ 12.input-numero/main.py
@@ -0,0 +1,29 @@
+from PySide6.QtWidgets import QApplication,   QMainWindow, QDoubleSpinBox
+from PySide6.QtCore import Qt
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.initialize()
+
+    def initialize(self):
+        self.number = QDoubleSpinBox()
+        self.setCentralWidget(self.number)
+        # self.number.setMinimum(55)
+        # self.number.setMaximum(1000)
+        self.number.setRange(55, 1000)
+        self.number.setSingleStep(0.4)
+        self.number.setSuffix("%")
+        self.number.valueChanged.connect(self.on_change)
+
+    def on_change(self, value):
+        print(value)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
