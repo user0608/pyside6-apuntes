d43fe6d43b0acb06e875e30a6cf7b9dea9fe69cf
Kevin Saucedo <user002_@outlook.com>
2023-09-03 18:31:47 -0500

    29. definiendo estilos

--- 29.estilos/main.py
+++ 29.estilos/main.py
@@ -41,12 +41,36 @@ class MainWindows(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
 
-    palette = QPalette()
-    palette.setColor(QPalette.ColorRole.Window, QColor(51, 51, 51))
-    palette.setColor(QPalette.ColorRole.WindowText, QColor(255, 255, 255))
+    dark_fusion = QPalette()
+    dark_fusion.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+    dark_fusion.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
+    dark_fusion.setColor(QPalette.ColorRole.Base, QColor(35, 35, 35))
+    dark_fusion.setColor(QPalette.ColorRole.AlternateBase, QColor(53, 53, 53))
+    dark_fusion.setColor(QPalette.ColorRole.ToolTipBase, QColor(25, 25, 25))
+    dark_fusion.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor.white)
+    dark_fusion.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
+    dark_fusion.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+    dark_fusion.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor.white)
+    dark_fusion.setColor(QPalette.ColorRole.BrightText, Qt.red)
+    dark_fusion.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
+    dark_fusion.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
 
-    app.setPalette(palette)
+    dark_fusion.setColor(
+        QPalette.ColorRole.HighlightedText, QColor(35, 35, 35))
+    dark_fusion.setColor(QPalette.ColorGroup.Active,
+                         QPalette.ColorRole.Button, QColor(53, 53, 53))
+    dark_fusion.setColor(QPalette.ColorGroup.Disabled,
+                         QPalette.ColorRole.ButtonText, Qt.GlobalColor.darkGray)
+    dark_fusion.setColor(QPalette.ColorGroup.Disabled,
+                         QPalette.ColorRole.WindowText, Qt.GlobalColor.darkGray)
+    dark_fusion.setColor(QPalette.ColorGroup.Disabled,
+                         QPalette.ColorRole.Text, Qt.GlobalColor.darkGray)
+    dark_fusion.setColor(QPalette.ColorGroup.Disabled,
+                         QPalette.ColorRole.Light, QColor(53, 53, 53))
+
+    app.setPalette(dark_fusion)
 
     windows = MainWindows()
     windows.show()
